a0431cdb89f1ec845b4165f7b9ee46c68c2c905c
aklukas <goldenshore8@gmail.com>
2018-08-28 08:46:16 -0700

    built and deployed to surge

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,10 +1,8 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
-import Post from './post/post';
 
 let commonModule = angular.module('app.common', [
-  Navbar,
-  Post
+  Navbar
 ])
   
 .name;
